e63af840bb9e98d00abd761c0c547ebd671eecfd
PerryCameron <pcameron28@yahoo.com>
2022-09-25 11:00:00 -0400

    First Commit

---
+++ src/main/java/label/perry/LabelTest.java
@@ -0,0 +1,8 @@
+package label.perry;
+
+public class LabelTest {
+    public static void main(String[] args) {
+        String lines[] = {"Joe Cameron","92","FM", "03/01/23"};
+        LabelPrinter.printMembershipLabel(lines);
+    }
+}
